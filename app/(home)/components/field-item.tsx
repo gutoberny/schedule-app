495717868744b73a770b5bcc6ad723dc761d5461
Gustavo Berny <pelotas.berny93@gmail.com>
2024-02-03 15:17:35 -0300

    star field

--- app/(home)/components/field-item.tsx
+++ app/(home)/components/field-item.tsx
@@ -1,6 +1,8 @@
+import { Badge } from "@/app/components/ui/badge";
 import { Button } from "@/app/components/ui/button";
 import { Card, CardContent } from "@/app/components/ui/card";
 import { Field } from "@prisma/client";
+import { StarIcon } from "lucide-react";
 import Image from "next/image";
 interface FieldItemProps {
   field: Field;
@@ -11,6 +13,12 @@ const FieldItem = ({ field }: FieldItemProps) => {
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
       <CardContent className="px-1 py-0">
         <div className="relative w-full h-[159px]">
+          <div className="absolute top-3 left-3 z-50">
+            <Badge variant="secondary" className="opacity-90 flex gap- items-center top-3 left-3">
+              <StarIcon size={12} className="fill-primary text-primary" />
+              <span className="text-xs">5,0</span>
+            </Badge>
+          </div>
           <Image className="rounded-2xl pt-1" width={0} height={0} sizes="100vw" src={field.imageUrl} alt="" fill style={{ objectFit: "cover" }} />
         </div>
         <div className="px-2 pb-3">
